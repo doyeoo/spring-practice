93831966794a0db6cfd64a55745d5b1c94dad980
doyeoo <ehdusla013@gmail.com>
2023-02-08 16:10:38 +0900

    feat: 엔티티 컬럼 추가

--- src/main/java/org/zerock/ex2/entity/Memo.java
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -8,9 +8,16 @@ import javax.persistence.*;
 @Entity
 @Table(name = "tbl_memo")
 @ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Memo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
 }
