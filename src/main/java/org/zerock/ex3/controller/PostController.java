1b99a7fda148731f1f8c0211b3eeba6b5e2db273
doyeoo <ehdusla013@gmail.com>
2023-02-10 21:13:32 +0900

    feat: 게시글 작성 컨트롤러

---
+++ src/main/java/org/zerock/ex3/controller/PostController.java
@@ -0,0 +1,31 @@
+package org.zerock.ex3.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.ex3.service.PostService;
+import org.zerock.ex3.dto.PostDTO;
+
+
+@Controller
+@RequestMapping("/posts")
+@Log4j2
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService service;
+
+    @PostMapping("/")
+    public ResponseEntity<Long> write(@RequestBody PostDTO postDTO) {
+        log.info(postDTO);
+
+        Long num = service.write(postDTO);
+
+        return new ResponseEntity<>(num, HttpStatus.OK);
+    }
+}
